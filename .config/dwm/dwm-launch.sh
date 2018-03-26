2ede5a9d3666c2fbd719bbfbd7d676609a958562
Michael Kuc <michaelkuc6@gmail.com>
2018-03-26 17:50:15 +0100

    Improved location of comment.

--- .config/dwm/dwm-launch.sh
+++ .config/dwm/dwm-launch.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
-# Log stderror to a file
 compton --backend glx --paint-on-overlay --glx-no-stencil --vsync opengl-swc --unredir-if-possible &
 export QT_QPA_PLATFORMTHEME="qt5ct"
+# Log stderror to a file
 dwm 2> ~/.dwm.log
 killall compton
