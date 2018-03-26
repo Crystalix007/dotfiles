f25e55aab83cd94dd2f5e6e14e8fcc21fa713d15
Michael Kuc <michaelkuc6@gmail.com>
2018-03-26 18:02:31 +0100

    Removed zsh dependency for launch and autostart scripts.

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 feh --bg-scale /usr/share/wallpapers/ColdRipple/contents/images/1920x1080.jpg
--- .config/dwm/dwm-launch.sh
+++ .config/dwm/dwm-launch.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/sh
 compton --backend glx --paint-on-overlay --glx-no-stencil --vsync opengl-swc --unredir-if-possible &
 export QT_QPA_PLATFORMTHEME="qt5ct"
 # Log stderror to a file
