1cb848cf606d9bc209ab7cbe24e7615e55922419
Michael Kuc <michaelkuc6@gmail.com>
2018-05-04 19:45:58 +0100

    Added slstatus launch to blocking script.

---
+++ .config/dwm/autostart-blocking.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This command will block, to add commands which follow this, fork this command
+# like, `slstatus &`. However, consider moving this to the autostart.sh file instead
+# then, as it will no longer be blocking.
+slstatus
